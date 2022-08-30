61b6ca92d63c2522db570de456e64b4755613cf4
Michael Keo <promichaelkeo@gmail.com>
2022-08-29 21:39:43 -0500

    changes

--- Pile.C
+++ Pile.C
@@ -1,5 +1,32 @@
 #include <stdio.h>
 
 int main(){
+
+    createPile();
+
+    int infinity = 0;
+    int i, j;
+
+    while(infinity == 0){
+        for(i = 0; i < 23; i++){
+            for(j = 0; j < 23; j++){
+                System.out.print(sandpile[i][j]);
+                }
+            System.out.println();
+        }
+        System.out.println();
+    }
     
+}
+
+void createPile(){
+    int sandpile[23][23];
+
+    int i, j;
+
+    for(i = 0; i < 23; i++){
+        for(j = 0; j < 23; j++){
+            sandpile[i][j] = 0
+        }
+    }
 }
